39283681fbdb657cec4068c10ff3241f0c5c6e14
Jessica Ladislau <paula.jfe@gmail.com>
2025-08-04 19:47:30 -0700

    changing alt text

--- src/components/AnimatedStacks.tsx
+++ src/components/AnimatedStacks.tsx
@@ -20,36 +20,36 @@ const AnimatedStacks = () => {
         <div className="marquee-container gap-12 h-[166px]">
             <div className="marquee-track flex">
                 <div className="marquee-content flex gap-12 pt-12 pb-12">
-                    <img key="1" src={figma} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="2" src={sql} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="3" src={node} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="4" src={javascript} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="5" src={typescript} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="6" src={html} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="7" src={css} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="8" src={reactjs} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="9" src={redux} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="10" src={git} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="11" src={elastic} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="12" src={dotnet} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="13" src={python} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="14" src={npm} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="15" src={jest} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="16" src={figma} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="17" src={sql} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="18" src={node} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="19" src={javascript} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="20" src={typescript} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="21" src={html} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="22" src={css} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="23" src={reactjs} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="24" src={redux} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="25" src={git} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="26" src={elastic} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="27" src={dotnet} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="28" src={python} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="29" src={npm} alt="J smile logo" style={{ height: '70px' }} />
-                    <img key="30" src={jest} alt="J smile logo" style={{ height: '70px' }} />
+                    <img key="1" src={figma} alt="Figma Icon" style={{ height: '70px' }} />
+                    <img key="2" src={sql} alt="SQL Icon" style={{ height: '70px' }} />
+                    <img key="3" src={node} alt="Node Icon" style={{ height: '70px' }} />
+                    <img key="4" src={javascript} alt="JS Icon" style={{ height: '70px' }} />
+                    <img key="5" src={typescript} alt="TS Icon" style={{ height: '70px' }} />
+                    <img key="6" src={html} alt="HTML Icon" style={{ height: '70px' }} />
+                    <img key="7" src={css} alt="CSS Icon" style={{ height: '70px' }} />
+                    <img key="8" src={reactjs} alt="React Icon" style={{ height: '70px' }} />
+                    <img key="9" src={redux} alt="Redux Icon" style={{ height: '70px' }} />
+                    <img key="10" src={git} alt="Git Icon" style={{ height: '70px' }} />
+                    <img key="11" src={elastic} alt="Elastic Icon" style={{ height: '70px' }} />
+                    <img key="12" src={dotnet} alt="dotNet Icon" style={{ height: '70px' }} />
+                    <img key="13" src={python} alt="Python Icon" style={{ height: '70px' }} />
+                    <img key="14" src={npm} alt="NPM Icon" style={{ height: '70px' }} />
+                    <img key="15" src={jest} alt="Jest Icon" style={{ height: '70px' }} />
+                    <img key="16" src={figma} alt="Figma Icon" style={{ height: '70px' }} />
+                    <img key="17" src={sql} alt="SQL Icon" style={{ height: '70px' }} />
+                    <img key="18" src={node} alt="Node Icon" style={{ height: '70px' }} />
+                    <img key="19" src={javascript} alt="JS Icon" style={{ height: '70px' }} />
+                    <img key="20" src={typescript} alt="TS Icon" style={{ height: '70px' }} />
+                    <img key="21" src={html} alt="HTML Icon" style={{ height: '70px' }} />
+                    <img key="22" src={css} alt="CSS Icon" style={{ height: '70px' }} />
+                    <img key="23" src={reactjs} alt="React Icon" style={{ height: '70px' }} />
+                    <img key="24" src={redux} alt="Redux Icon" style={{ height: '70px' }} />
+                    <img key="25" src={git} alt="Git Icon" style={{ height: '70px' }} />
+                    <img key="26" src={elastic} alt="Elastic Icon" style={{ height: '70px' }} />
+                    <img key="27" src={dotnet} alt="dotNet Icon" style={{ height: '70px' }} />
+                    <img key="28" src={python} alt="Python Icon" style={{ height: '70px' }} />
+                    <img key="29" src={npm} alt="NPM Icon" style={{ height: '70px' }} />
+                    <img key="30" src={jest} alt="Jest Icon" style={{ height: '70px' }} />
                 </div>
             </div>
         </div>
@@ -61,36 +61,36 @@ export default AnimatedStacks;
 {
     /* <div className="marquee-container relative overflow-hidden whitespace-nowrap w-full flex flex-row gap-12 items-center justify-center h-[160px]">
     <div className="marquee inline-flex w-max animate-marquee animate-scroll gap-12">
-        <img key="1" src={figma} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="2" src={sql} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="3" src={node} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="4" src={javascript} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="5" src={typescript} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="6" src={html} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="7" src={css} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="8" src={reactjs} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="9" src={redux} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="10" src={git} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="11" src={elastic} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="12" src={dotnet} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="13" src={python} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="15" src={jest} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="14" src={npm} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="16" src={figma} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="17" src={sql} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="18" src={node} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="19" src={javascript} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="20" src={typescript} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="21" src={html} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="22" src={css} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="23" src={reactjs} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="24" src={redux} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="25" src={git} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="26" src={elastic} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="27" src={dotnet} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="28" src={python} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="29" src={npm} alt="J smile logo" style={{ height: '70px' }} />
-        <img key="30" src={jest} alt="J smile logo" style={{ height: '70px' }} />
+        <img key="1" src={figma} alt="Figma Icon" style={{ height: '70px' }} />
+        <img key="2" src={sql} alt="SQL Icon" style={{ height: '70px' }} />
+        <img key="3" src={node} alt="Node Icon" style={{ height: '70px' }} />
+        <img key="4" src={javascript} alt="JS Icon" style={{ height: '70px' }} />
+        <img key="5" src={typescript} alt="TS Icon" style={{ height: '70px' }} />
+        <img key="6" src={html} alt="HTML Icon" style={{ height: '70px' }} />
+        <img key="7" src={css} alt="CSS Icon" style={{ height: '70px' }} />
+        <img key="8" src={reactjs} alt="React Icon" style={{ height: '70px' }} />
+        <img key="9" src={redux} alt="Redux Icon" style={{ height: '70px' }} />
+        <img key="10" src={git} alt="Git Icon" style={{ height: '70px' }} />
+        <img key="11" src={elastic} alt="Elastic Icon" style={{ height: '70px' }} />
+        <img key="12" src={dotnet} alt="dotNet Icon" style={{ height: '70px' }} />
+        <img key="13" src={python} alt="Python Icon" style={{ height: '70px' }} />
+        <img key="15" src={jest} alt="Jest Icon" style={{ height: '70px' }} />
+        <img key="14" src={npm} alt="NPM Icon" style={{ height: '70px' }} />
+        <img key="16" src={figma} alt="Figma Icon" style={{ height: '70px' }} />
+        <img key="17" src={sql} alt="SQL Icon" style={{ height: '70px' }} />
+        <img key="18" src={node} alt="Node Icon" style={{ height: '70px' }} />
+        <img key="19" src={javascript} alt="JS Icon" style={{ height: '70px' }} />
+        <img key="20" src={typescript} alt="TS Icon" style={{ height: '70px' }} />
+        <img key="21" src={html} alt="HTML Icon" style={{ height: '70px' }} />
+        <img key="22" src={css} alt="CSS Icon" style={{ height: '70px' }} />
+        <img key="23" src={reactjs} alt="React Icon" style={{ height: '70px' }} />
+        <img key="24" src={redux} alt="Redux Icon" style={{ height: '70px' }} />
+        <img key="25" src={git} alt="Git Icon" style={{ height: '70px' }} />
+        <img key="26" src={elastic} alt="Elastic Icon" style={{ height: '70px' }} />
+        <img key="27" src={dotnet} alt="dotNet Icon" style={{ height: '70px' }} />
+        <img key="28" src={python} alt="Python Icon" style={{ height: '70px' }} />
+        <img key="29" src={npm} alt="NPM Icon" style={{ height: '70px' }} />
+        <img key="30" src={jest} alt="Jest Icon" style={{ height: '70px' }} />
     </div>
 </div>; */
 }
